9826a46be635ea5450d72c7e7bf705e5f279d778
Gaston Gonzalez <gaston@perfectsensedigital.com>
2014-11-22 09:46:58 -0800

    Deleted Mongoose since we do not need it

--- ghome-node/server.js
+++ ghome-node/server.js
@@ -7,11 +7,7 @@ var api = require('./app/api');
 
 // configuration ===========================================
 	
-// config files
-var db = require('./config/db');
-
 var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
@@ -29,5 +25,5 @@ require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-exports = module.exports = app; 						// expose app
+console.log('GHome started on port ' + port);
+exports = module.exports = app; // expose app
