0cbd1af6408cd4c1b56f2e0b0a698f3a46387396
Gaston Gonzalez <gaston@perfectsensedigital.com>
2014-11-22 10:54:54 -0800

    Added initial support for a sprinkler API

--- ghome-node/app/api-routes.js
+++ ghome-node/app/api-routes.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var apis = [ 'sprinklers' ]; // available APIs
+
 router.get('/', function(req, res) {
     var json = {};
+    json.apis = apis;
     res.json(json);
 });
 
+router.route('/sprinklers')
+   .get(function(req, res) {
+    var json = {zones: []};
+    for (i = 0; i < 3; i++) {
+	json['zones'].push({
+	    zone: i,
+            location: 'front',
+	    status: 'on'
+	});
+    }
+    res.json(json);
+    });
+
+router.route('/sprinklers/:zone_id')
+   .get(function(req, res) {
+	var json = {
+	    zone: req.params.zone_id,
+            location: 'front',
+	    status: 'on'
+	}
+	res.json(json);
+    });
+
+router.route('/sprinklers/:zone_id/:zone_state')
+   .get(function(req, res) {
+	var json = {
+	    zone: req.params.zone_id,
+            location: 'front',
+	    status: req.params.zone_state
+	}
+	res.json(json);
+    });
 module.exports = router;
