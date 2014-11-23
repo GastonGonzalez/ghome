490b1e3632c4acfd07b65131f000e67a99390cdd
Gaston Gonzalez <gaston@perfectsensedigital.com>
2014-11-23 07:04:06 -0800

    Connected Angular to backend

--- ghome-node/app/api-routes.js
+++ ghome-node/app/api-routes.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 
 var apis = [ 'sprinklers' ]; // available APIs
@@ -10,26 +11,43 @@ router.get('/', function(req, res) {
 });
 
 router.route('/sprinklers')
-   .get(function(req, res) {
-    var json = {zones: []};
-    for (i = 0; i < 3; i++) {
-	json['zones'].push({
-	    zone: i,
-            location: 'front',
-	    status: 'on'
+   .get(function(serverReq, serverRes) {
+
+	var proxyJson = {};
+        http.get("http://192.168.1.50", function(res) {
+	    console.log("Got response: " + res.statusCode);
+
+	    res.on('data', function (chunk) {
+		console.log('Sprinkler response: ' + chunk);
+		proxyJson = JSON.parse(chunk);
+
+		var json = {zones: []};
+		for (i = 0; i < proxyJson.zones.length; i++) {
+		    json['zones'].push({
+			zone: proxyJson.zones[i].zoneNum,
+			status: proxyJson.zones[i].zoneState === 1 ? 'on' : 'off'
+		    });
+		}
+		serverRes.json(json);
+	    });
+	}).on('error', function(e) {
+	    console.log("Got error: " + e.message);
 	});
-    }
-    res.json(json);
+
+
     });
 
 router.route('/sprinklers/:zone_id')
-   .get(function(req, res) {
-	var json = {
-	    zone: req.params.zone_id,
-            location: 'front',
-	    status: 'on'
-	}
-	res.json(json);
+   .get(function(serverReq, serverRes) {
+
+        http.get("http://192.168.1.50?toggle=" + serverReq.params.zone_id, function(res) {
+	    console.log("Got response: " + res.statusCode);
+	    serverRes.json({});
+	}).on('error', function(e) {
+	    console.log("Got error: " + e.message);
+	    serverRes.json({});
+	});
+
     });
 
 router.route('/sprinklers/:zone_id/:zone_state')
--- ghome-node/public/js/controllers/SprinklerCtrl.js
+++ ghome-node/public/js/controllers/SprinklerCtrl.js
@@ -1,5 +1,12 @@
 angular.module('SprinklerCtrl', []).controller('SprinklerController', function($scope, $http) {
 
+    $scope.handleZone = function(zoneNum) {
+	$http.get('/api/sprinklers/' + zoneNum)
+	    .success(function(data) {
+		console.log("Success!");
+	    });
+    };
+
     $http.get('/api/sprinklers').success(function(data) {
 	$scope.zones = data.zones;
     });
