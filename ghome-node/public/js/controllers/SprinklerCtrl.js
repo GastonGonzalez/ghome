ab83357c1db2f5166c974ed4dcb6bc5b65853029
Gaston Gonzalez <gaston@perfectsensedigital.com>
2014-11-23 07:35:38 -0800

    Added support to refesh zone list on button click

--- ghome-node/public/js/controllers/SprinklerCtrl.js
+++ ghome-node/public/js/controllers/SprinklerCtrl.js
@@ -1,11 +1,18 @@
-angular.module('SprinklerCtrl', []).controller('SprinklerController', function($scope, $http) {
+angular.module('SprinklerCtrl', []).controller('SprinklerController', function($rootScope, $scope, $http) {
 
     $scope.handleZone = function(zoneNum) {
 	$http.get('/api/sprinklers/' + zoneNum)
 	    .success(function(data) {
 		console.log("Success!");
+		$rootScope.$broadcast('updateZones');
 	    });
     };
+
+    $scope.$on("updateZones",function(zoneNum){
+	$http.get('/api/sprinklers').success(function(data) {
+	    $scope.zones = data.zones;
+	});
+    });
 
     $http.get('/api/sprinklers').success(function(data) {
 	$scope.zones = data.zones;
